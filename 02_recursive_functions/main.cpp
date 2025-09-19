24db63a7b67e23b720d5da37bbce43fc7305e311
sssctkt <justaskaisssctkt@gmail.com>
2025-09-19 10:06:32 -0400

    recursive function (ascending)

--- 02_recursive_functions/main.cpp
+++ 02_recursive_functions/main.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 using namespace std;
-int factorial_nonrecursive(int term);
-int factorial_recursive(int term);
-int main(){
-    int answer;
-    answer=factorial_recursive(6);
-    cout<<answer;
+void print_ascending(int number);
+
+int main() {
+    int number=5;
+    print_ascending(number);
     return 0;
 }
-int factorial_nonrecursive(int term){
-    int result=1;
-    for (int z=1; z<=term; z++)
-    {
-        result*=z;
-    }
-    
-    return result;
-}
-int factorial_recursive(int term){
-    //exit clause
-    if (term==1){
-        return 1;
+
+void print_ascending(int number){
+    if (number==1){
+        cout<<number<<endl;
     }
     else{
-        return term*factorial_recursive(term-1);
+        print_ascending(number-1);
+        cout<<number<<endl;
     }
 }
