8a4cd79e846e3ce8b8d544e3c7bf2830671848a6
sssctkt <justaskaisssctkt@gmail.com>
2025-10-06 08:30:09 -0400

    vectors and flexible array

---
+++ 03_05_vectors(optional)/main.cpp
@@ -0,0 +1,16 @@
+//today's lesson vectors
+//vectors will not be on any evaluations 
+//however, during evaluations, if you think vectors are easier
+//you can use them on any evaluations
+#include <iostream>
+using namespace std;
+int main()
+{
+    //recap: "flexible array"
+    int length=3;
+    int a[length]; //you can create array with size 3 in this way
+    a[0]=23;
+    cout<<a[0];
+
+    return 0;
+}
