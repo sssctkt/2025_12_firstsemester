d5c4a936e420ec0a4b03f32e1c33a4203cc8e599
sssctkt <justaskaisssctkt@gmail.com>
2025-10-06 08:39:31 -0400

    how to sort vectors

--- 03_05_vectors(optional)/main.cpp
+++ 03_05_vectors(optional)/main.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
 int main()
 {
-    vector<int> res={3, 1, 2, 3, 3, 3,};
-    //in vector we have something called iterator to go through the vector
-    vector<int>::iterator it = res.begin(); //beginning of the vector
-    for(it ; it != res.end(); )//end means the end of the vector
-    {
-        if (*it==3){ //we need * to show the element
-        it = res.erase(it);
-            //this is pretty complicated, but using erase will return the next valid
-            //iterator
-        }
-       else{
-           it++; //it will go to the next iterator
-       }
-       //the reason why we can't use it++ at the beginner of the for loop is that
-       //we can't go beyond the end of the vectors
-       //since it++ at the control mechanism of the for loop means it will do ++ regardless
-    }
-    //put out the outcome
-    it=res.begin();
-    for (it; it!=res.end(); it++){
+    vector<int> a={3, 2, 1};
+    sort(a.begin(), a.end());
+    //method 1 to go through the vector
+    vector<int>::iterator it = a.begin();
+    for (it; it!=a.end(); it++){
         //this time we can put it++ at the top because we are not planning to delete anything
         cout<<*it<<" ";
     }
+    cout<<endl;
+    //method 2 using old notation
+    for (int i=0; i<a.size(); i++){
+        cout<<a[i];
+    }
+
+    return 0;
 }
