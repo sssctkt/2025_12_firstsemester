7726296e9ccf132b31a2feeee3f28f0440e8d587
sssctkt <justaskaisssctkt@gmail.com>
2025-10-21 09:46:15 -0400

    pointer and address

--- 04_pointers/pointers.cpp
+++ 04_pointers/pointers.cpp
@@ -1,25 +1,17 @@
 
 #include <iostream>
-
 using namespace std;
 int main()
-{   
-    //regularly:
-    int x;
-    x=5; //the variable x holds the value of 5;
-    //& symbol has two meaning, 1. aliasing 2: address of the variable 
-    //what is a pointer, a pointer is a variable, that instead of holding a value
-    //it holds the address of the other variable!
-    int  *b; //create a pointer b, it can hold the address of the integer variable
-    b=&x; //the pointer b holds the address of x;
-    cout<<x<<endl;
-    cout<<&x<<endl; //address of x
-    cout<<b<<endl;
-    cout<<&b<<endl; //address of the pointer b
-    //dereferencing
-    cout<<*b<<endl; //it will print the value of the address of the variable that it 
-    //is holding
+{
+    int x=3;
+    int *b;
+    b=&x;
+    cout<<b<<endl; //address of x 
+    cout<<&x<<endl; //address of x 
+    cout<<x<<endl; //3 
+    cout<<*b<<endl; //dereferencing-->the value of the address the pointer is holding 
+    cout<<*&x<<endl; //3
+   
+
     return 0;
 }
-
-
