fcf489486a49561a9c3e002f772d6d5292dab524
sssctkt <justaskaisssctkt@gmail.com>
2025-10-22 08:38:38 -0400

    pointers application

--- 04_pointers/pointers.cpp
+++ 04_pointers/pointers.cpp
@@ -1,19 +1,33 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
+//the function must have no return type 
+//1. use aliasing
+void collectname(string &a);
+//2. use pointers
+void collectname(string *a);
 int main()
 {
-    int x=3, y=62;
-    int *b, *c=&y;
-
-    b=&x;
-    b=c;
-    *c=100;
-    cout<<*b<<endl;
-    cout<<*c<<endl;
-    cout<<x<<endl;
-    cout<<y<<endl;
-   
+    /*
+    pointer stage 1 will be completed today
+    */
+    string name="";
+    //call a function to collect the name
+    //collectname(name);
+    collectname(&name);
+    cout<<name;
+    //int *q=&x;
+    
 
     return 0;
+}
+void collectname(string &a){
+    cout<<"Enter name";
+    getline(cin, a);
+}
+
+void collectname(string *a){
+    cout<<"Enter name";
+    getline(cin, *a);
 }
