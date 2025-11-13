5b7a2b424a518673ca55bf11c2f2f1ed660345f3
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 10:40:55 -0500

    assigning structure

--- 05_structure/structure_basic.cpp
+++ 05_structure/structure_basic.cpp
@@ -7,23 +7,29 @@ struct student{
     //array-->store elements of one type 
     //structure-->store elements of many type
     int grade;
-    char lastname[50];
+    char lastname[50]; //array
     string firstname;
     int student_number;
-
 
 };
 int main()
 {
-    student s1[11];
-    s1[0].firstname="CT";
-    s1[0].student_number=9931;
-    s1[0].grade=80;
-    strcpy(s1[0].lastname,"Kwan");
-    
-    
-    cout<<s1[0].firstname<<" "<<s1[0].student_number<<endl;
-    cout<<s1[0].lastname;
-    
+    student s1, s2, *s3;
+    s1.firstname="CT";
+    s1.student_number=9931;
+    s1.grade=80;
+    strcpy(s1.lastname,"Kwan");
+    //for structure, they want to treat it like a regular variable
+    //EVEN if you have array inside a structrue
+    s2=s1;
+    s3=&s1;
+    strcpy(s1.lastname, "Quan");
+    s1.grade=20;
+    cout<<s1.firstname<<" "<<s1.student_number<<endl;
+    cout<<s1.lastname<<" "<<s1.grade<<endl;
+    cout<<s2.firstname<<" "<<s2.student_number<<endl;
+    cout<<s2.lastname<<" "<<s2.grade<<endl;
+    cout<<s3->firstname<<" "<<s3->student_number<<endl;
+    cout<<s3->lastname<<" "<<s3->grade<<endl;
     return 0;
 }
