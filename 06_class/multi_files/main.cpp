d068f619fb48c61b21e5be00698d842623f9c8d1
sssctkt <justaskaisssctkt@gmail.com>
2025-12-09 09:42:30 -0500

    dec 8

--- 06_class/multi_files/main.cpp
+++ 06_class/multi_files/main.cpp
@@ -9,7 +9,14 @@ int main()
     
     //class and object-oriented programming 
     //what is object-oriented programming-->an object can have its own data and functions
-    fraction f3, f4,answer;
+    int x{3}; //this is how we also can initialize the variables
+    cout<<x<<endl;
+    fraction f3, f4,answer, f5{7,8}; //we need something called "custom constructor"
+    f3.getvalue(); //I want it to be 0/1
+    cout<<endl;
+    f5.getvalue();
+    cout<<endl;
+    //to make it work
     f3.setvalue(2,5); //object f3 that calls the function
     f4.setvalue(4,5);
     answer=f3.displaybigger(f4);
@@ -22,7 +29,17 @@ int main()
     cout<<endl;
     answer=f3-(f4);
     cout<<"The difference is: ";
-    
     answer.getvalue();
+    cout<<endl;
+    
+    answer=f5+x;
+    cout<<"The sum is: ";
+    answer.getvalue();
+    cout<<endl;
+    
+    answer=x+f5;
+    cout<<"The sum is: ";
+    answer.getvalue();
+    cout<<endl;
     return 0;
 }
